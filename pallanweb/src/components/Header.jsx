2067a18249a285b0d70ec17f2793b02692fb2324
Ogola <brianochiengogola78@gmail.com>
2022-09-21 23:15:44 +0300

    changes on navbar

--- pallanweb/src/components/Header.jsx
+++ pallanweb/src/components/Header.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from  'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <div className="navbar">
-    {['sm'].map((expand) => (
-        <Navbar key={expand} bg="transparent" expand={expand} className="mb-3 mt-3 justify-content-center 
-        ">
+    <>
+      {[ 'md'].map((expand) => (
+        <Navbar key={expand} bg="success" expand={expand} className=" m-5" d-flex>
           <Container fluid>
-            <Navbar.Brand href="#">Pallan systems</Navbar.Brand>
+            <Navbar.Brand href="#">Pallan Systems</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -23,30 +20,14 @@ function Header() {
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Pallan
+                  PSL
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">About</Nav.Link>
-                  <Nav.Link href="#action1">Business</Nav.Link>
-                  <Nav.Link href="#action1">Mission</Nav.Link>
-                  <Nav.Link href="#action1">Contact</Nav.Link>
-                  <Nav.Link href="#action1">Gallery</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Links</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  
                 </Nav>
                 <Form className="d-flex">
                   <Form.Control
@@ -55,15 +36,15 @@ function Header() {
                     className="me-2"
                     aria-label="Search"
                   />
-                  <Button variant="outline-success">Search</Button>
+                  <Button variant="outline-info">Search</Button>
                 </Form>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
         </Navbar>
-      ))}
-    </div>
+      ))};
+    </>
   )
 }
 
-export default Header
+export default Header;
---
+++ pallanweb/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+
+function Home() {
+  return (
+    <div className="m-5 p-3 mt-5 " >
+      <Button>Know more about psl</Button>
+    </div>
+  )
+}
+
+export default Home;
